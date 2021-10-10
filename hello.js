bd9eeca4514a808f32b3f305a7bab4a036997821
philipuuu <topgun0415@naver.com>
2021-10-10 21:33:00 +0900

    Feat: Button added

--- hello.js
+++ hello.js
@@ -1,17 +1,10 @@
 /** @format */
 
-<<<<<<< HEAD
 const a = 'Hello javascript!';
 console.log(a);
 
 const calcul = () => {
   console.log(a);
 };
-=======
-const a = 'Hello world!';
-var hello = () => {
-  console.log(a);
-};
 
-hello();
->>>>>>> MyPractice
+const fix = 'commit message try';
